fc89682cb361cc14ed242821a8321b38ac59fafb
Rémi Brun <remi.brun@appsolu.net>
2023-10-25 11:06:36 +0200

    Resume de l'upload

--- upload-map.sh
+++ upload-map.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #rsync -avx -e ssh --progress europe_france-gh.tar.xz root@stockage.taxi.appsolu.net:/var/www/stockage/static/routing
-rsync -avx -e ssh --progress europe.pbf root@stockage.taxi.appsolu.net:/var/www/stockage/static/routing
+rsync -av -e "ssh -o PubkeyAcceptedKeyTypes=+ssh-dss" -P --append-verify europe.pbf root@stockage.taxi.appsolu.net:/var/www/stockage/static/routing
